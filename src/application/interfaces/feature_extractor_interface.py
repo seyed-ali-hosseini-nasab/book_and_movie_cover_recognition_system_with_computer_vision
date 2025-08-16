110daac8b277ef08491c016d57262732737b67e1
seyed-ali-hosseini-nasab <sahnnet.working@gmail.com>
2025-08-16 15:23:29 +0330

    ♻️ Add ExtractFeatureData data class

--- src/application/interfaces/feature_extractor_interface.py
+++ src/application/interfaces/feature_extractor_interface.py
@@ -1,9 +1,17 @@
 from abc import ABC, abstractmethod
-from typing import Tuple, List, Optional
+from dataclasses import dataclass
+from typing import List
 import numpy as np
 import cv2
 
+
+@dataclass
+class ExtractFeatureData:
+    keypoints: List[cv2.KeyPoint]
+    descriptors: np.ndarray | None
+
+
 class IFeatureExtractor(ABC):
     @abstractmethod
-    def extract_features(self, image: np.ndarray) -> Tuple[List[cv2.KeyPoint], Optional[np.ndarray]]:
+    def extract_features(self, image: np.ndarray) -> ExtractFeatureData:
         pass
