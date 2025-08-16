54d153a40ac1e9859ad30f73325ba3157b531137
seyed-ali-hosseini-nasab <sahnnet.working@gmail.com>
2025-08-16 15:09:44 +0330

    ♻️ Rename book image file and optimize SIFT extractor

--- src/infrastructure/feature_extractors/sift_extractor.py
+++ src/infrastructure/feature_extractors/sift_extractor.py
@@ -1,14 +1,13 @@
-from typing import Tuple, List, Optional
 import numpy as np
 import cv2
-from src.application.interfaces.feature_extractor_interface import IFeatureExtractor
+from src.application.interfaces.feature_extractor_interface import IFeatureExtractor, ExtractFeatureData
 
 
 class SIFTExtractor(IFeatureExtractor):
     def __init__(self):
         self.sift = cv2.SIFT_create()
 
-    def extract_features(self, image: np.ndarray) -> Tuple[List[cv2.KeyPoint], Optional[np.ndarray]]:
+    def extract_features(self, image: np.ndarray) -> ExtractFeatureData:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         keypoints, descriptors = self.sift.detectAndCompute(gray, None)
-        return keypoints, descriptors
+        return ExtractFeatureData(keypoints, descriptors)
