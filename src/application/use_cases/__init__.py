e8b8442826ef75e7c66abfcd7e118d0d50ada137
seyed-ali-hosseini-nasab <sahnnet.working@gmail.com>
2025-08-16 17:34:20 +0330

    🔥 Remove deprecated process_video.py

--- src/application/use_cases/__init__.py
+++ src/application/use_cases/__init__.py
@@ -6,7 +6,6 @@ from .frame_processing import *
 from .image_processing.find_matching_book_movie import FindMatchingBookMovieUseCase
 from .image_processing.overlay_book_cover import OverlayBookCoverUseCase
 from .video_processing.process_input_video import ProcessInputVideoUseCase
-from .video_processing.process_video import ProcessVideoUseCase
 
 __all__ = [
     # Image Processing
@@ -15,7 +14,6 @@ __all__ = [
 
     # Video Processing
     'ProcessInputVideoUseCase',
-    'ProcessVideoUseCase',
     'BookDetectorInVideo',
     'TrailerFrameLoader',
 
--- src/application/use_cases/video_processing/__init__.py
+++ src/application/use_cases/video_processing/__init__.py
@@ -1,11 +1,9 @@
 from .process_input_video import ProcessInputVideoUseCase
-from .process_video import ProcessVideoUseCase
 from .book_detector_in_video import BookDetectorInVideo
 from .trailer_frame_loader import TrailerFrameLoader
 
 __all__ = [
     'ProcessInputVideoUseCase',
-    'ProcessVideoUseCase',
     'BookDetectorInVideo',
     'TrailerFrameLoader'
 ]
